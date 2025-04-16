0553cc15a71fcf7db27e21d8db5b8aceb20e8a9d
Geovanny Araujo <trejogeovannyaraujo@gmail.com>
2025-04-15 18:00:28 -0600

    Funcion eleiminar imagenes de la seleccion

--- app/features/panoramas/views.py
+++ app/features/panoramas/views.py
@@ -16,7 +16,19 @@ def get_panoramas(request):
 def add_panoramas(request):
     panoramas = request.FILES.getlist('images')
     
-    
+
     for panorama in panoramas:
         print("Panorama: ", panorama.name)
     return  HttpResponse("Panoramas added successfully!")
+
+
+
+def get_decimal_from_dms(dms, ref):
+    degrees = dms[0][0] / dms[0][1]
+    minutes = dms[1][0] / dms[1][1]
+    seconds = dms[2][0] / dms[2][1]
+    decimal = degrees + (minutes / 60.0) + (seconds / 3600.0)
+    if ref in [b'S', b'W']:
+        decimal = -decimal
+        
+    return decimal
--- app/static/js/panoramas/dropzone.js
+++ app/static/js/panoramas/dropzone.js
@@ -1,9 +1,17 @@
 const dropzone = document.getElementById("dropzone");
 const input = document.getElementById("images");
 const preview = document.getElementById("preview");
-const cancelButton = document.getElementById("btn-cancel");
 
 dropzone.addEventListener("click", () => input.click());
+
+dropzone.addEventListener("dragover", (e) => {
+  e.preventDefault();
+  dropzone.classList.add("bg-light");
+});
+
+dropzone.addEventListener("dragleave", () => {
+  dropzone.classList.remove("bg-light");
+});
 
 dropzone.addEventListener("drop", (e) => {
   e.preventDefault();
@@ -17,23 +25,34 @@ input.addEventListener("change", () => {
 
 function showPreview(files) {
   preview.innerHTML = "";
-  Array.from(files).forEach((file) => {
+  Array.from(files).forEach((file, index) => {
     const reader = new FileReader();
     reader.onload = function (e) {
       const col = document.createElement("div");
-      col.className = "col-4 mb-3";
+      col.className = "col-4 mb-3 position-relative";
+
       col.innerHTML = `
+          <div class="position-relative">
+            <button type="button" class="btn-close position-absolute top-0 end-0 m-2" aria-label="Cerrar"></button>
             <img src="${e.target.result}" class="img-fluid rounded border shadow" alt="preview">
-          `;
+          </div>
+        `;
+
+      // Evento para eliminar la imagen
+      col.querySelector(".btn-close").addEventListener("click", () => {
+        col.remove();
+        // Si deseas también eliminar el archivo del input, necesitarías gestionarlo con un FileList personalizada (más complejo)
+      });
+
       preview.appendChild(col);
     };
     reader.readAsDataURL(file);
   });
 }
 
-cancelButton.addEventListener("click", () => {
+function cancelUpload() {
   const preview = document.getElementById("preview");
   const input = document.getElementById("images");
   preview.innerHTML = "";
   input.value = null;
-});
+}
