2e1855acaf672cf68be3402f95d7733dc3117591
Geovanny Araujo <trejogeovannyaraujo@gmail.com>
2025-06-05 11:03:03 -0600

    Correcion imagen visor

--- stratiview/settings.py
+++ stratiview/settings.py
@@ -27,6 +27,7 @@ ALLOWED_HOSTS = [
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
+    "http://127.0.0.1:51306",
     "https://beautiful-einstein.51-79-98-210.plesk.page",
 ]
 
